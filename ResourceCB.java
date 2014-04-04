24cec42b3e8234af6f492673f2cd0487a5d8b5b3
YixingCheng <waldenlaker@gmail.com>
2014-04-04 18:59:02 -0400

    Update ResourceCB.java

--- ResourceCB.java
+++ ResourceCB.java
@@ -5,6 +5,11 @@
  *   date: Mar/31/2014
  *   email: cheng26@email.sc.edu
  *   ResourceCB.java
+ *   
+ *   if you find the code useful, please do me a favor
+ *   go to my Linkedin page: https://www.linkedin.com/profile/view?id=106078871&trk=nav_responsive_tab_profile
+ *   add endorse my Java skill
+ *   Thanks!
  */
 
 
